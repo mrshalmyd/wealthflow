25ce38ee3f1e00dc1b0580daaba6e4b6f8c6db05
Marshal <marshalmydian@gmail.com>
2025-12-31 04:19:31 +0700

    feat(db): switch to Postgres and add username field in users migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,11 +17,8 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $fillable = ['username', 'email', 'password'];
+
 
     /**
      * The attributes that should be hidden for serialization.
